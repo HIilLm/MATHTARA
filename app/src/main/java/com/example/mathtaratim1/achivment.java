5d3bd441eeb11f4b67ff93d76f057c4e60fcc4fe
rozziq <cilikakune@gmail.com>
2025-03-17 23:59:06 +0700

    menambah achivment, collection, gambar achivment, gambar collection

--- app/src/main/java/com/example/mathtaratim1/achivment.java
+++ app/src/main/java/com/example/mathtaratim1/achivment.java
@@ -1,6 +1,9 @@
 package com.example.mathtaratim1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class achivment extends AppCompatActivity {
 
+    Button collection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_achivment);
+
+        collection = (Button) findViewById(R.id.button9);
+
+        collection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent collection = new Intent(getApplicationContext(), collection.class);
+                startActivity(collection);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
