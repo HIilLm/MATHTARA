6b4d3c8386037d7613ff6f7d540d11bfdfb109c1
HIilLm <hoshihole@gmail.com>
2025-03-19 01:10:32 +0700

    register fixed

--- app/src/main/java/com/example/mathtaratim1/Register.java
+++ app/src/main/java/com/example/mathtaratim1/Register.java
@@ -3,6 +3,7 @@ package com.example.mathtaratim1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -31,5 +32,14 @@ public class Register extends AppCompatActivity {
                     startActivity(intent);
                 }
         });
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
