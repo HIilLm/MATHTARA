fba1d9ffad320656e5ea3fa54e6e76d1d07c3dda
HIilLm <hoshihole@gmail.com>
2025-03-19 00:35:38 +0700

    add home menu

--- app/src/main/java/com/example/mathtaratim1/prestory.java
+++ app/src/main/java/com/example/mathtaratim1/prestory.java
@@ -1,6 +1,11 @@
 package com.example.mathtaratim1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class prestory extends AppCompatActivity {
 
+    Button goBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_prestory);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        goBack = (Button) findViewById(R.id.back);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(prestory.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
