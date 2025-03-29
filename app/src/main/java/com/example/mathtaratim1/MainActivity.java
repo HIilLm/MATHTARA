05289530774c5956ae170c3baab8e18c7aa51d5c
rozziq <cilikakune@gmail.com>
2025-03-29 11:39:19 +0700

    menjalankan fitur beli gems

--- app/src/main/java/com/example/mathtaratim1/MainActivity.java
+++ app/src/main/java/com/example/mathtaratim1/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -16,7 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements
+        NavigationBarView.OnItemSelectedListener, ShopFragment.OnGemPurchaseListener {
 
     private BottomNavigationView bottomNavigationView;
     private ShopFragment shopFragment = new ShopFragment();
@@ -24,12 +26,18 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     private HomeFragment homeFragment = new HomeFragment();
     private CollectionFragment collectionFragment = new CollectionFragment();
     private FavoriteFragment favoriteFragment = new FavoriteFragment();
+
+    private int totalGems = 0; // Awalnya 0
+    private TextView gemTextView; // TextView untuk menampilkan jumlah gems
+
     ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -37,44 +45,50 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
         });
 
         imageView = findViewById(R.id.avatar);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
-                startActivity(intent);
-            }
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
         });
 
+        gemTextView = findViewById(R.id.gem); // Hubungkan dengan TextView navbar
+        updateGemText(); // Tampilkan jumlah awal gems
 
         bottomNavigationView = findViewById(R.id.bottomView);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
-
-        // **Set item default yang dipilih di BottomNavigationView**
         bottomNavigationView.setSelectedItemId(R.id.home);
-
         bottomNavigationView.setOnItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            int itemId = item.getItemId();
-            if (itemId == R.id.shop) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, shopFragment).commit();
-                return true;
-            } else if (itemId == R.id.gear) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, gearFragment).commit();
-                return true;
-            } else if (itemId == R.id.home) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
-                return true;
-            } else if (itemId == R.id.collection) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, collectionFragment).commit();
-                return true;
-            } else if (itemId == R.id.favorite) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, favoriteFragment).commit();
-                return true;
-            }
-            return false;
+        int itemId = item.getItemId();
+        if (itemId == R.id.shop) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, shopFragment).commit();
+            return true;
+        } else if (itemId == R.id.gear) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, gearFragment).commit();
+            return true;
+        } else if (itemId == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+            return true;
+        } else if (itemId == R.id.collection) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, collectionFragment).commit();
+            return true;
+        } else if (itemId == R.id.favorite) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, favoriteFragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    // Menerima jumlah gems dari ShopFragment
+    @Override
+    public void onGemPurchased(int gems) {
+        totalGems += gems; // Tambahkan jumlah gems
+        updateGemText(); // Update TextView di navbar
+    }
+
+    private void updateGemText() {
+        gemTextView.setText(String.valueOf(totalGems)); // Perbarui teks di navbar
     }
 }
