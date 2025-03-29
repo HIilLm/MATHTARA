775ca01d9d0d30fdd41abee96b89e3dbf38e80d0
HIilLm <hoshihole@gmail.com>
2025-03-29 12:15:36 +0700

    Intent cerita

--- app/src/main/java/com/example/mathtaratim1/cerita1s1.java
+++ app/src/main/java/com/example/mathtaratim1/cerita1s1.java
@@ -1,6 +1,9 @@
 package com.example.mathtaratim1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,25 @@ public class cerita1s1 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button terima = findViewById(R.id.button4);
+        Button tolak = findViewById(R.id.button5);
+
+        terima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toS2 = new Intent(cerita1s1.this, cerita1s2.class);
+                startActivity(toS2);
+            }
+        });
+
+        tolak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toS3 = new Intent(cerita1s1.this, cerita1s3.class);
+                startActivity(toS3);
+            }
         });
     }
 }
